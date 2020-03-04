57878c6426f1459ed7e4dc1408a742322cc568b5
FirozKabir65 <firoj.kabir007@gmail.com>
2020-03-04 21:03:54 +0100

    JavaScript C06: Clock done

--- Exercise Files/06_04/script.js
+++ Exercise Files/06_04/script.js
@@ -2,9 +2,16 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
+let hrPosition = 10;
+let minPosition = 110;
+let secPosition = 210;
+
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hours: " + hr + " Minutes: " + min + " Seconds: " + sec);
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
